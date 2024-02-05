06cab57355e0d42addc8a9798421665af739dfe2
4Miguel2 <delgdevoleper@gmail.com>
2024-02-05 17:39:26 -0300

    imports arrumados

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
